b0c7f072aa420406454b03f5891cadec577acaea
Paul Bouman <bouman@ese.eur.nl>
2021-07-05 15:21:24 +0200

    Update course settings
    Setting up the course settings to be able to do some development. These will change when we move to an EUR-based URL!

--- course-settings.js
+++ course-settings.js
@@ -1,31 +1,31 @@
 const courseSettings = {
   language: "en",
   name: "Introduction To Programming",
-  siteUrl: "https://rowtricker.github.io",
-  subtitle: "An introductory programming course on Java",
-  slug: "java-programming-i",
-  tmcCourse: "java-programming-i",
-  quizzesId: "6a635a2d-4e4b-49cc-8487-8d9cab6a74e7",
+  siteUrl: "https://econometricinstitute.github.io/feb22012-programming-website/",
+  subtitle: "A course on Object Oriented Programming in Java",
+  slug: "java-programming-ii",
+  tmcCourse: "java-programming-ii",
+  quizzesId: "1d10a591-8704-467f-bfb0-7edc8b22b490",
   tmcOrganization: "mooc",
   bannerPath: "banner.svg",
   sidebarEntries: [
     {
       title: "About the course",
-      path: "/",
+      path: "/feb22012-programming-website/",
     },
     {
       title: "Sessions and Support",
-      path: "/sessions-and-support",
+      path: "/feb22012-programming-website/sessions-and-support",
     },
     {
       title: "Grading and exams",
-      path: "/grading-and-exams",
+      path: "/feb22012-programming-website/grading-and-exams",
     },
     {
       title: "Style guide",
-      path: "/style-guide",
+      path: "/feb22012-programming-website/style-guide",
     },
-    { separator: true, title: "Java Programming I" },
+    { separator: true, title: "Java Programming II" },
   ],
   sidebarFuturePages: [
   ], 
